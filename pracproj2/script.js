e79b85f03be5dbad3a8e9607ca71b98582323c51
nikita-dev <nikita.baraiya@devdigital.com>
2025-05-30 17:14:20 +0530

    JS update

---
+++ pracproj2/script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+        "pk.eyJ1IjoibmlraS0xMjMyMiIsImEiOiJjbWI5ODRhOTAwdGV1MmlzZWdycnhqNzd0In0.BzxZP8a9fYwEubid6ZvFCQ";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-71.060982, 42.35725],
+        zoom: 18,
+      });
